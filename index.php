e4992cf518cb0eb6077954115a672fca203844c1
Zuliatun Istiqomah <zuliatunisti@gmail.com>
2022-10-24 10:56:44 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	
+	echo "Hello World!<br>";
+
+	$nama = "Zuliatun Istiqomah";
+	$umur = 20;
+
+	echo"Nama saya <strong>$nama</strong>, saya berusia $umur tahun, hehe<br>";
+
+	$namaAdik = "umi khanifah";
+	$umurAdik = "17";
+
+	echo"Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+	$selisihumur = $umur -$umurAdik;
+	echo "Selisih usia saya dan adik sya adalah $selisihumur tahun."
+?>
