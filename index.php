2f4c0d4c2298e80ad48fa07dbc7c7999daae92c6
Zuliatun Istiqomah <zuliatunisti@gmail.com>
2022-11-24 20:36:50 +0700

    add file update dan delete

--- index.php
+++ index.php
@@ -5,23 +5,24 @@ $con = mysqli_connect ("localhost","root","","fakultas");
 // 2. Cek koneksi dengan MySQL 
 if (mysqli_connect_errno()){
     echo "Koneksi gagal ". mysqli_connect_errno();
+    exit();
 }else {
     echo "Koneksi berhasil";
 }
 
 // 3. membaca data dari table mysql.
-$query = "SELECT * FROM mahasiswa";
+$sql = "SELECT * FROM mahasiswa";
 
-// 4. tampilkan data, dengan menjalankan sql query
-$result = mysqli_query($con,$query);
-$mahasiswa = []; 
-if ($result){
-    // tampilkan data satu per satu
+// 4. tampilkan data, cek apakah query bisa dijalankan
+$mahasiswa = [];
+if $result = (mysqli_query($con,$sql)){
+    //tampilkan satu persatu
     while($row = mysqli_fetch_assoc($result)){
-    $mahasiswa[] = $row;
+        $mahasiswa[] = $row;
+        }
+        mysqli_free_result($result);
     }
-    mysqli_free_result($result);
-}
+
 
 // 5. tutup koneksi mysql
 mysqli_close($con);
@@ -38,16 +39,31 @@ mysqli_close($con);
 </head>
 <body>
     <h1>Data Mahasiswa</h1>
+    <a href="insert.php">Tambah Data</a>
     <table border="1" syle="width:100%;">
         <tr>
             <th>NIM</th>
             <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Action</th>
         </tr>
-        <?php foreach($mahasiswa as $value): ?>
+        <?php foreach($mahasiswa as $row): ?>
         <tr>
-            <td><?php echo $value["nim"]; ?></td>
-            <td><?php echo $value["nama"]; ?></td>
+            <td><?= $row['nim'] ?></td>
+            <td><?= $row['nama'] ?></td>
+            <td><?= $row['jenis_kelamin'] ?></td>
+            <td><?= $row['tempat_lahir'] ?></td>
+            <td><?= $row['tanggal_lahir'] ?></td>
+            <td><?= $row['alamat'] ?></td>
+            <td>
+                <a href="update.php?id=<?= $row['id'] ?>" >Edit</a> | 
+                <a href="delete.php?id=<?= $row['id'] ?>" >Delete</a>
+                </td>
         </tr>
-        <?php endforeach; ?>  
+        <?php endforeach; ?>
+    </table>
 </body> 
 </html>
